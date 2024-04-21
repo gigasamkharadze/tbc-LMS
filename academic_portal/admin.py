efd2d4e8fd0f7b41b95039a7ed36408a726c2033
gigasamkharadze <gigasamkharadzee@example.com>
2024-04-21 23:11:29 +0400

    feat: admin models

--- academic_portal/admin.py
+++ academic_portal/admin.py
@@ -1,3 +1,44 @@
 from django.contrib import admin
-
+from academic_portal.models import Lecturer, Student, Course, Faculty
 # Register your models here.
+
+
+class LecturerAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'email', 'date_of_birth', 'courses_display')
+    search_fields = ('first_name', 'last_name', 'email')
+
+    @staticmethod
+    def courses_display(obj):
+        return ', '.join([course.title for course in obj.courses.all()])
+    courses_display.short_description = 'Courses'
+
+
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'email', 'date_of_birth', 'faculty', 'year_of_study', 'courses_display')
+    search_fields = ('first_name', 'last_name', 'email')
+
+    @staticmethod
+    def courses_display(obj):
+        return ', '.join([course.title for course in obj.courses.all()])
+    courses_display.short_description = 'Courses'
+
+
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ('code', 'title', 'description', 'syllabus')
+    search_fields = ('code', 'title')
+
+
+class FacultyAdmin(admin.ModelAdmin):
+    list_display = ('name', 'courses_display')
+    search_fields = ('name',)
+
+    @staticmethod
+    def courses_display(obj):
+        return ', '.join([course.title for course in obj.courses.all()])
+    courses_display.short_description = 'Courses'
+
+
+admin.site.register(Lecturer, LecturerAdmin)
+admin.site.register(Student, StudentAdmin)
+admin.site.register(Course, CourseAdmin)
+admin.site.register(Faculty, FacultyAdmin)
