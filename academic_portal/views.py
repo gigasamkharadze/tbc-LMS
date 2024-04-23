82e2affdfb6ff3a1d76f6e25f3b4442d4c5df431
gigasamkharadze <gigasamkharadzee@example.com>
2024-04-23 17:37:18 +0400

    handle not-student users

--- academic_portal/views.py
+++ academic_portal/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import redirect, render, get_object_or_404
+from rest_framework.response import Response
 
 from academic_portal.models import Course
 
@@ -15,16 +16,18 @@ def add_course(request):
             course = request.POST.get('course')
             course = get_object_or_404(Course, code=course)
             user.student.courses.add(course)
-        #     go to the same page
             return redirect('academic_portal:add_course')
         else:
-            student = user.student
-            return render(request, 'add_course.html', {
-                'courses': student.courses.all(),
-                'first_name': student.first_name,
-                'last_name': student.last_name,
-                'faculty': student.faculty,
-                'all_courses': student.faculty.courses.all()
-            })
+            if hasattr(user, 'student'):
+                student = user.student
+                return render(request, 'add_course.html', {
+                    'courses': student.courses.all(),
+                    'first_name': student.first_name,
+                    'last_name': student.last_name,
+                    'faculty': student.faculty,
+                    'all_courses': student.faculty.courses.all()
+                })
+            else:
+                return render(request, 'not_student.html')
     else:
         return redirect('login')
