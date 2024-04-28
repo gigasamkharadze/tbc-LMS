135c22913b84ecf0941012de4b1489b623a56bd2
gigasamkharadze <samkharadze.giga@kiu.edu.ge>
2024-04-28 15:48:37 +0400

    migration added for lecturer model

--- academic_portal/forms.py
+++ academic_portal/forms.py
@@ -3,9 +3,11 @@ from academic_portal.models import Assignment
 
 
 class AssignmentForm(forms.ModelForm):
+
     class Meta:
         model = Assignment
         fields = ['title', 'description', 'due_date', 'course']
         widgets = {
             'due_date': forms.DateInput(attrs={'type': 'date'}),
         }
+
